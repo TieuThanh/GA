1f56b6c49bb5ae45025533117a78f219c5b9b7f5
TieuThanh <17521056@gm.uit.edu.vn>
2020-10-14 22:51:32 +0700

    update

--- TH/BT1/BT1.py
+++ TH/BT1/BT1.py
@@ -194,8 +194,21 @@ def lowerMRPS(N,target,num_weights,code ,fitness_function, random_seed = 1752105
             break
     return N_upper, number_of_evaluations/10
 
-def draw(l,results,name1, name2):
-    pass
+def draw(l,result1, result2, code, name):
+    y1 = np.log(result1)
+    y2 = np.log(result2)
+    x = np.log(l)
+    y_err = 1
+    fig, ax = plt.subplots()
+    ax.errorbar(x[:len(y1)],y1,yerr = y_err, fmt = '-o', label='1X')
+    ax.errorbar(x[:len(y2)],y2,yerr = y_err, fmt = '-o', label='UX')
+    ax.set_xlabel('log(L)')
+    ax.set_ylabel('log({})'.format(name))
+    ax.set_title('Kết quả cho thuật toán {} {}'.format(code, name))
+    plt.legend(loc='upper left')
+    fig.savefig('Kết quả cho thuật toán {} {}'.format(code, name))
+    plt.show()
+
 
 def bisection(target, num_weights, code, fitness_function, random_seed=17521056, N = 4):
 
@@ -222,7 +235,8 @@ def writeResults(code,fitness_function):
             MRPS, avg_number_of_evaluations = bisection(target=target, num_weights=num_weights, 
                                                             code=code, random_seed=random_seed,N = N,fitness_function = fitness_function)
             string = 'bisection: ' + str(i) + ', MRPS: '+ str(MRPS) + ', avg_number_of_evaluations: ' + str(avg_number_of_evaluations)+'\n'
-            if MRPS == 0: return 0
+            if MRPS == 0:
+                return 0
             f.write(string)
             random_seed += 10
             print('MRPS: ',MRPS)
@@ -230,10 +244,56 @@ def writeResults(code,fitness_function):
         print('completed l = {}'.format(l))
         l *= 2
 
+def readData(fname):
+    f = open(fname,'r')
+    data = f.readlines()
+    MRPS = []                           # Biến lưu giá trị MRPS của mỗi bisection
+    number_of_evaluations = []          # Biến lưu giá trị của số lần gọi hàm trong 1 bisection
+    avg_MRPS = []                       # Biến lưu giá trị trung bình của MRPS trong 10 lần bisection tương ứng với 1 kích thước
+    avg_number_of_evaluations = []      # Biến lưu giá trị trung bình của số lần gọi hàm trong 10 lần bisection tương ứng với 1 kích thước
+
+
+    # Duyệt qua từng dòng của dữ liệu
+    for line in data:
+        line = line.strip().split(",")
+        if len(line)>1:
+
+            # Lấy giá trị của MRPS tương ứng với 1 bisection
+            MRPS.append(int(line[1].split(':')[-1]))
+
+            # Lấy giá trị của số lần gọi hàm tương ứng với 2 bisection
+            number_of_evaluations.append(float(line[2].split(':')[-1]))
+
+    # Tính trung bình trên mỗi kích thước ( Mỗi kích thước chạy trên 10 bisection)
+
+    start = 0
+    end = 10
+    while (end <= len(MRPS)):
+        avg_MRPS.append(sum(MRPS[start:end])/10)
+        avg_number_of_evaluations.append(round(sum(number_of_evaluations[start:end])/10,1))
+        start += 10
+        end += 10
+    return avg_MRPS, avg_number_of_evaluations
+
+
 if __name__ == "__main__":
     fitness_functions = ['onemax','trap5']
     codes = ['UX','1X']
     for fitness_function in fitness_functions:
         for code in codes:
             writeResults(code,fitness_function)
+
+
+    # tương ứng theo thứ tự là MRPS và số lần gọi hàm
+    result1, result2 = readData('1X_onemax_results.txt')
+    result3, result4 = readData('UX_onemax_results.txt')
+    result5, result6 = readData('1X_trap5_results.txt')
+    result7, result8 = readData('UX_trap5_results.txt')
+    l = [10,20,40,80,160]
+    draw(np.array(l), np.array(result1),np.array(result3),'One Max','avg_MRPS')
+    draw(np.array(l), np.array(result2),np.array(result4),'One Max','avg_number_of_evaluations')
+    draw(np.array(l), np.array(result5),np.array(result7),'Trap 5','avg_MRPS')
+    draw(np.array(l), np.array(result6),np.array(result8),'Trap 5','avg_number_of_evaluations')
+
+
     
